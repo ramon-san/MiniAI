776b7304da4e97afc683f4295713c30aaae15419
Ramon-san <ramon.orraca@correo.uia.mx>
2021-04-22 16:03:09 -0600

    Initial Commit

---
+++ MiniAI/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MiniAI
+//
+//  Created by Ramon Orraca on 22/04/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
